5555fcb47babd90323626a565da30ff63f191e4f
xuzeyun <jonehsu@163.com>
2019-05-04 23:20:54 +0800

    add header

--- src/views/home/components/Header.vue
+++ src/views/home/components/Header.vue
@@ -1,26 +1,40 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <!-- 返回 -->
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <!-- 输入框 -->
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>输入城市/游玩主题
+    </div>
+    <!-- 城市 -->
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
 <script>
   export default{
     name: 'HomeHeader'
-  }
+}
 </script>
 
 <style lang="stylus" scoped>
+  @import '~@/assets/styles/varibles.styl'
   .header
     display: flex
     line-height:.86rem
-    background: #00bcd4
+    background: $bgColor
     color:#fff
     .header-left
       width:.64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-input
       flex: 1
       height: .64rem
