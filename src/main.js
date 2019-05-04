7d7286783034e725eacc42ad9fc3faa44bdb6be7
xuzeyun <jonehsu@163.com>
2019-05-04 12:56:24 +0800

    add files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 初始化 fastClick
+fastClick.attach(document.body)
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: {App},
+//   template: '<app/>'
+// })
